f81fa259565da6d48d60597d151e264ba82d6f66
JAJJovellanos <47479695+JAJJovellanos@users.noreply.github.com>
2019-05-16 16:46:12 +0200

    Add files via upload

--- layout/header.php
+++ layout/header.php
@@ -32,7 +32,7 @@
           <ul class="navbar-nav ml-auto">
 			
             <!--<li class="nav-item active">
-              <a class="nav-link" href="/www/loginregister-master/">Inicio
+              <a class="nav-link" href="/www/">Inicio
                 <span class="sr-only">(current)</span>
               </a>
             </li>-->
